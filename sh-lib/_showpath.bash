b6a4c74182abacb9d8d906141e84f759aac68328
Martin D Kealey <martin@kurahaupo.gen.nz>
2003-10-21 12:44:24 +1300

    from kurahaupo.gen.nz/web-pages/~martin/shell-profile/.sh-lib

---
+++ sh-lib/_showpath.bash
@@ -0,0 +1,14 @@
+function _showpath {
+    local _var=${1:?'Missing param'}
+    eval local _path=\"\$$_var\"
+    local IFS=:
+    for _p in $_path
+    do
+        case $_p/ in
+        $HOME/*) _p='~'${_p#$HOME} ;;
+        /)       _p=. ;;
+        esac
+        echo "    $_p"
+    done
+}
+_provides _showpath
