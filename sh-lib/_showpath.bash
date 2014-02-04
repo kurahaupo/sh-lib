0e0a145d3bfcac5d3455fb7a93a1109f75c48b1b
Martin D Kealey <martin@kurahaupo.gen.nz>
2014-02-04 17:30:32 +1300

    from backup/support.quakers.nz/home/martin/.sh-lib

--- sh-lib/_showpath.bash
+++ sh-lib/_showpath.bash
@@ -1,12 +1,20 @@
 function _showpath {
     local _var=${1:?'Missing param'}
     eval local _path=\"\$$_var\"
-    local IFS=:
+    local IFS=: _p _h _u
     for _p in $_path
     do
         case $_p/ in
         $HOME/*) _p='~'${_p#$HOME} ;;
-        /)       _p=. ;;
+        /home/*/)
+                _h=$_p/
+                _h=${_h%%"${_h#/home/*/}"}
+                _h=${_h%/}
+                _u=${_h##/home/}
+                IFS=: read _u _ _ _ _ _h _ < <(getent passwd "$_u") &&
+                _p='~'$_u${_p#"$_h"}
+                ;;
+        /)      _p=. ;;
         esac
         echo "    $_p"
     done
