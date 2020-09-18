c37c40154738033acf279f4b25f8bd4f92ed63f4
Martin D Kealey <martin@kurahaupo.gen.nz>
2020-09-18 13:38:45 +1000

    from git/noenv/.git-lib-bash

--- sh-lib/ip2num.bash
+++ sh-lib/ip2num.bash
@@ -1,25 +1,29 @@
 
 function ip2num {
     local i=$1 r=0 j
-    local -a q=( 24 16 8 0 ) x=( $(IFS=. ; echo $i) )
-    for ((j=0;j<4;j++))
-    do
-        ((r|=x[j]<<q[j]))
+    local -a q x
+    q=( 24 16 8 0 )
+    IFS=. read -ra x <<<"$i"
+    for ((j=0;j<4;j++)) do
+        ((r|=x[j]<<(~j<<3&24)))
     done
-    echo $r
+    printf '%u\n' $r
 }
-
-function num2ip {
-    local n=$1
-    echo $((n>>24&255)).$((n>>16&255)).$((n>>8&255)).$((n&255))
-}
+_provides ip2num
 
 function prefix2netmask {
     local n
     (( n=~0<<(32-$1) ))
     num2ip $n
 }
-
-_provides num2ip
-_provides ip2num
 _provides prefix2netmask
+
+function num2ip {
+    local n=$1
+    printf '%u.%u.%u.%u\n' \
+            $((n>>24&255)) \
+            $((n>>16&255)) \
+            $((n>> 8&255)) \
+            $((n    &255))
+}
+_provides num2ip
