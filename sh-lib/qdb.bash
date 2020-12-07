c35b03654086efd80ee445b30a861b7a850e5606
Martin D Kealey <martin@kurahaupo.gen.nz>
2020-12-07 15:00:27 +1000

    from backup/ds920.kohi.sig.nz/volume1/homes_1/martin/.sh-lib

---
+++ sh-lib/qdb.bash
@@ -0,0 +1,21 @@
+qdb() {
+    QDB_DBTYPE='mysql' \
+    QDB_DBNAME='quakers' \
+    QDB_HOST='::1' \
+    QDB_PORT=33060 \
+    QDB_USER='quakers_user' \
+    QDB_PASSFILE=/run/user/$UID/qdb_secret \
+    "$@"
+}
+
+(
+    QDB_PASSFILE=/run/user/$UID/qdb_secret
+    QDB_PASS='XVcWY9Syrg6qnFLu' 
+    [[ -s "$QDB_PASSFILE" ]] || {
+        mkdir -p "${QDB_PASSFILE%/*}"
+        printf '%s\n' "$QDB_PASS" > "$QDB_PASSFILE"
+        chmod 400 "$QDB_PASSFILE"
+    }
+)
+
+_provides qdb
