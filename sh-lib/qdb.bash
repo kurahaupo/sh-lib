551a3d5e885b9b56c4216304dfb2bcbe0b2b2dda
Martin D Kealey <martin@kurahaupo.gen.nz>
2020-12-07 15:00:27 +1000

    from backup/ds920.kohi.sig.nz/volume1/homes_1/martin/.sh-lib

--- sh-lib/qdb.bash
+++ sh-lib/qdb.bash
@@ -10,7 +10,7 @@ qdb() {
 
 (
     QDB_PASSFILE=/run/user/$UID/qdb_secret
-    QDB_PASS='XVcWY9Syrg6qnFLu'
+    QDB_PASS='XVcWY9Syrg6qnFLu' 
     [[ -s "$QDB_PASSFILE" ]] || {
         mkdir -p "${QDB_PASSFILE%/*}"
         printf '%s\n' "$QDB_PASS" > "$QDB_PASSFILE"
