a2f811e1c7fc9901d017637e30b2d94df6579519
Martin D Kealey <martin@kurahaupo.gen.nz>
2022-02-23 19:49:40 +1000

    qdb.bash add qdb6

--- sh-lib/qdb.bash
+++ sh-lib/qdb.bash
@@ -2,15 +2,25 @@ qdb() {
     QDB_DBTYPE='mysql' \
     QDB_DBNAME='quakers' \
     QDB_HOST='::1' \
-    QDB_PORT=33060 \
-    QDB_USER='quakers_user' \
+    QDB_PORT=33020 \
+    QDB_USER='quakers' \
+    QDB_PASSFILE=/run/user/$UID/qdb_secret \
+    "$@"
+}
+
+qdb6() {
+    QDB_DBTYPE='mysql' \
+    QDB_DBNAME='quakers_d6' \
+    QDB_HOST='::1' \
+    QDB_PORT=34021 \
+    QDB_USER='quakers_d6' \
     QDB_PASSFILE=/run/user/$UID/qdb_secret \
     "$@"
 }
 
 (
     QDB_PASSFILE=/run/user/$UID/qdb_secret
-    QDB_PASS='XVcWY9Syrg6qnFLu'
+    QDB_PASS='XVcWY9Syrg6qnFLu' 
     [[ -s "$QDB_PASSFILE" ]] || {
         mkdir -p "${QDB_PASSFILE%/*}"
         printf '%s\n' "$QDB_PASS" > "$QDB_PASSFILE"
@@ -18,4 +28,4 @@ qdb() {
     }
 )
 
-_provides qdb
+_provides qdb qdb6
