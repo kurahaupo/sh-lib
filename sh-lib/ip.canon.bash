75390e3caa33a7bbf85b21a7bb5429057051fab7
Martin D Kealey <martin@kurahaupo.gen.nz>
2023-01-27 03:26:23 +1000

    ip.canon.bash: initial version of check_ipv6_address

--- sh-lib/ip.canon.bash
+++ sh-lib/ip.canon.bash
@@ -210,3 +210,194 @@ ip.canon() {
 
 _provides ip._get_parts ip.ll_parts ip.v4_parts ip.v6_parts ip.canon_v4 ip.canon_v6 ip.canon
 
+return
+
+# :<<\_END
+
+#
+# This is a valid POSIX shell function, which does not have general regex, only
+# glob matching.
+#
+check_ipv6_address() {
+
+    local colons=0
+    case $1 in
+        #
+        # Immediately invalid if:
+        #   - anything but colons and hex digits
+        #   - more than 4 digits per segment (any run of 5 non-colons)
+        #   - multiple empty segments
+        #   - starts or ends with a single colon
+        #   - too many segments (more than 8 colons)
+        #
+
+        *[!:0-9a-f]*            | \
+        *[!:][!:][!:][!:][!:]*  | \
+        *:::*       | *::*::*   | \
+        :[!:]*      | *[!:]:    | \
+        *:*:*:*:*:*:*:*:*:*     ) return 1 ;;
+
+        #
+        # Count how many colons, and validate:
+        #    - 3 to 8 - possibly valid
+        #    - ≤ 2 (except ::) - invalid
+        #
+        *:*:*:*:*:*:*:*:*       ) colons=8 ;;
+        *:*:*:*:*:*:*:*         ) colons=7 ;;
+        *:*:*:*:*:*:*           ) colons=6 ;;
+        *:*:*:*:*:*             ) colons=5 ;;
+        *:*:*:*:*               ) colons=4 ;;
+        *:*:*:*                 ) colons=3 ;;
+        *:*:*                   ) colons=2 ;;
+        *                       ) return 1
+    esac
+
+    #
+    # If non-zero, $compression indicates how many segments have been replaced
+    # by a single ::
+    # If it's at the start or end, include the leading or trailing 0 segment in
+    # the compression count (which is why it's arguably permissible to have
+    # eight colons when it starts or ends with a compression).
+    #
+    local lcomp=0 ; case $1 in ::*) lcomp=1 ; esac
+    local rcomp=0 ; case $1 in *::) rcomp=1 ; esac
+    local compression=0 ; case $1 in *::*) compression=$((8-colons+lcomp+rcomp)) ; esac
+
+    case $colons/$compression in
+
+        #
+        # If there's no compression then there must be exactly 7 colons, and if
+        # requested there should be no zeroes.
+        #
+        7/0 )   case :$1: in *:0:0:* | :*:0:*: ) (( IPV6_REQUIRE_MINIMAL )) && return 1 ;;
+                             :0:* | *:0:       ) (( IPV6_REQUIRE_MINIMAL_EDGE &&
+                                                  ! IPV6_FORBID_MINIMAL_EDGE )) && return 1 ; esac ;;
+        ?/0 )   return 1 ;;
+
+        #
+        # Valid if using :: to "compress" a single leading trailing zero segment:
+        #
+        # Many people consider this to be ugly and useless, since it could just
+        # as well be written ":0", so if you can treat this case as invalid by
+        # setting IPV6_FORBID_MINIMAL_EDGE=1 (or simply deleting the following
+        # line).
+        #
+
+        8/1 )   (( IPV6_FORBID_MINIMAL_EDGE )) && return 1 ;;
+
+    esac
+
+    #
+    # If there's a longer sequence of zero segments than is already
+    # compressed, or one of equal length but nearer the start, then that's
+    # (optionally) rejected
+    #
+
+    (( IPV6_REQUIRE_MINIMAL )) &&
+    case $compression in
+
+        1 ) case :$1: in *:0:0:*       )  return 1 ;;
+                         *:0:*::*:     ) (( IPV6_ALLOW_DISORDERED_MINIMAL )) || return 1 ; esac ;;
+        2 ) case :$1: in *:0:0:0:*     ) return 1 ;;
+                         *:0:0:*::*:   ) (( IPV6_ALLOW_DISORDERED_MINIMAL )) || return 1 ; esac ;;
+        3 ) case :$1: in *:0:0:0:0:*   ) return 1 ;;
+                         *:0:0:0:*::*: ) (( IPV6_ALLOW_DISORDERED_MINIMAL )) || return 1 ; esac ;;
+
+    esac
+
+    case $1 in
+
+        *:*:*:*:*:*:*:*:*:*     ) return 1 ;;
+        ?*:?*:?*:?*:?*:?*:?*::  ) (( IPV6_FORBID_MINIMAL_EDGE &&
+                                   ! IPV6_REQUIRE_MINIMAL_EDGE )) && return 1 ;;
+
+        #
+        # Valid if exactly 8 segments, all non-empty
+        #   - re-check for too many colons, but this time invalid if 9 (or more) segments; otherwise
+        #   - valid if 8 (or more) segments, all non-empty
+        #
+
+        *:*:*:*:*:*:*:*:*       )
+    case $1 in
+    esac
+
+                                    return 1 ;;
+        ?*:?*:?*:?*:?*:?*:?*:?* ) true  ;;
+
+        #
+        # Fewer than 8 segments can only be valid if there's an empty segment.
+        #
+        # But then the fun begins:
+
+        #
+        # The following are required by the standard when outputting IPv6
+        # addresses, but they're not ambiguous, so there's no harm in accepting
+        # them as input. But if you're paranoid, you can set IPV6_REQUIRE_MINIMAL=1
+        # to disallow them.
+        #
+        #   - no leading zeroes within segments;
+        #   - maximal compression (can't have :: followed or preceded by a 0 segment);
+        #   - optimal compression (must compress the longest run of 0 segments exists, and where
+        #       there's a choice, the first one);
+        #
+        # Detecting suboptimal compression is tricky; we can't just count the
+        # colons and subtract from 7, because a leading or trailing ::
+        # represents one more segment than an internal ::
+        #
+        #   0:0 without ::
+        #   0 before trailing :: compressing 1 segment (leaving 8 colons)
+        #       0:*:*:*:*:*:*::
+        #       *:0:*:*:*:*:*::
+        #       *:*:0:*:*:*:*::
+        #       *:*:*:0:*:*:*::
+        #       *:*:*:*:0:*:*::
+        #       *:*:*:*:*:0:*::
+        #
+        #   0:0 before :: compressing 2 segments (leaving 7 segments, 6 colons)
+        #       0:0:*:*:*:*:*
+        #       ?*:0:0:*:*:*:*
+        #       ?*:?*:0:0:*:*:*
+        #
+        #   0:0:0 before :: compressing 3 segments (leaving 6 segments, 5 colons)
+        #       0:0:0:*:*:*:*
+        #       ?*:0:0:0:*:*:*
+        #
+        #   0:0:0:0 anywhere
+        #       *:0:0:0:0
+        #       *:0:0:0:0:*
+        #       0:0:0:0:*
+        #
+        #  Because we've already excluded 0 adjacent to a ::, we can then consider:
+        #
+        #
+
+        *::* )
+            local colons=${1//[!:]} ; colons=${#colons}
+            case $1 in
+                ::*
+
+        #
+        # Strict mode: forbid suboptimal compression
+        #
+        0[!:]*      | *:0[!:]*  | \
+        0::*        | *:0::*    | \
+        *::0        | *::0:*    ) (( IPV6_REQUIRE_MINIMAL )) && return 1 ;;&
+
+        *:0:0:0:0               | \
+        *:0:0:0:0:*             | \
+        0:0:0:0:*               | \
+        0:0:0:*:*:*             | \
+        *:0:0:0:*:*             | \
+        *:*:0:0:*:*:*           | \
+        *:0:0:*:*:*:*           | \
+        0:0:*:*:*:*:*           | \
+        ) (( IPV6_REQUIRE_MINIMAL )) && return 1 ;;&
+                                # If we get past all that, it's valid
+                                true  ;;
+
+        *                       ) false
+
+    esac
+}
+
+#_END
