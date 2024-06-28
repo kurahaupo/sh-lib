4ef3bf89f6374a82ba8264528efa32347c70e592
Martin D Kealey <martin@kurahaupo.gen.nz>
2024-06-29 11:26:57 +1200

    cluck.bash: reformat width

--- sh-lib/cluck.bash
+++ sh-lib/cluck.bash
@@ -98,21 +98,21 @@ EndOfHelp
                 [SUCCESS]=0 [OK]=0 [PASS]=0
                 [FAILURE]=1 [FAIL]=1
                 [USAGE]=64 [DATAERR]=65 [NOINPUT]=66 [NOUSER]=67 [NOHOST]=68
-                [UNAVAILABLE]=69 [SOFTWARE]=70 [OSERR]=71 [OSFILE]=72 [CANTCREAT]=73
-                [IOERR]=74 [TEMPFAIL]=75 [PROTOCOL]=76 [NOPERM]=77 [CONFIG]=78
-                [BUG]=96 [INTERNAL]=96
-                [NOEXEC]=127
-                [BROKEN]=255
+                [UNAVAILABLE]=69 [SOFTWARE]=70 [OSERR]=71 [OSFILE]=72
+                [CANTCREAT]=73 [IOERR]=74 [TEMPFAIL]=75 [PROTOCOL]=76
+                [NOPERM]=77 [CONFIG]=78
+                [BUG]=96 [INTERNAL]=96 [NOEXEC]=127 [BROKEN]=255
             )
         fi
         if (( ${#SIG_values[@]} == 0 )); then
             # Linux-specific version; may not work elsewhere.
             declare -A SIG_values=(
-                [HUP]=1 [FPE]=8 [STKFLT]=16 [XCPU]=24 [INT]=2 [KILL]=9 [CHLD]=17
-                [XFSZ]=25 [QUIT]=3 [USR1]=10 [CONT]=18 [VTALRM]=26 [ILL]=4
-                [SEGV]=11 [STOP]=19 [PROF]=27 [TRAP]=5 [USR2]=12 [TSTP]=20
-                [WINCH]=28 [ABRT]=6 [PIPE]=13 [TTIN]=21 [IO]=29 [IOT]=6 [ALRM]=14
-                [TTOU]=22 [PWR]=30 [BUS]=7 [TERM]=15 [URG]=23 [SYS]=31
+                [HUP]=1 [FPE]=8 [STKFLT]=16 [XCPU]=24 [INT]=2 [KILL]=9
+                [CHLD]=17 [XFSZ]=25 [QUIT]=3 [USR1]=10 [CONT]=18 [VTALRM]=26
+                [ILL]=4 [SEGV]=11 [STOP]=19 [PROF]=27 [TRAP]=5 [USR2]=12
+                [TSTP]=20 [WINCH]=28 [ABRT]=6 [PIPE]=13 [TTIN]=21 [IO]=29
+                [IOT]=6 [ALRM]=14 [TTOU]=22 [PWR]=30 [BUS]=7 [TERM]=15 [URG]=23
+                [SYS]=31
             )
         fi
         ((_debug)) && {
