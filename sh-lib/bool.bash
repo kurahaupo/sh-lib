434187af207e0fba035cb0a45aa9fb199ae7f8c0
Martin D Kealey <martin@kurahaupo.gen.nz>
2024-07-05 14:23:21 +1200

    bool.bash - avoid exiting an interactive shell

--- sh-lib/bool.bash
+++ sh-lib/bool.bash
@@ -1,18 +1,40 @@
-_=${true=1}${false=0}
-
-[[ $true:$false != 1:0 ]] || {
-    require confess
-    confess -e EX_SOFTWARE
-    exit 70
-}
-
-readonly -i true false
-
-if [[ -n $true$false ]]
+#
+# Create "true" and "false" constants.
+#
+# Treat inability to set correct values as a fatal error.
+#
+if [[ $true:$false = 1:0 ]]
 then
+    # readonly without options is safe (and idempotent)
+    readonly true false
 else
+
+    # if these are already set and readonly, these will (rightly) provoke an error
+    true=1
+    false=0
+
+    readonly true false
+
+    if [[ $true:$false != 1:0 ]]
+    then
+        require cluck
+        cluck "Non-standard 'true' and/or 'false'"
+        declare -p true false
+    fi
+
+    if ! ( (( true )) ) ||
+         ( (( false )) )
+    then
+        # -q warn
+        cluck -q "Unusable value for 'true' or 'false'"
+        if [[ $- != *[il]* ]] || (( SHLVL > 1 ))
+        then
+            exit 70
+        fi
+    fi
+
 fi
 
-#_provides true
-#_provides false
+_provides true
+_provides false
 _provides bool
