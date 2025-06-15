f982787673d160646c2b34b71ba2d9f2dfeeb061
Martin D Kealey <martin@kurahaupo.gen.nz>
2025-06-15 21:56:09 +1000

    whitespace

--- sh-lib/_setpath.bash
+++ sh-lib/_setpath.bash
@@ -1,18 +1,20 @@
-function _setpath {
-    [[ -n ${true+_}  ]] || local -ri true=1  || { return ; echo >&2 ERROR cannot set true=1 in _setpath ; }
-    [[ -n ${false+_} ]] || local -ri false=0 || { return ; echo >&2 ERROR cannot set false=0 in _setpath ; }
-    (( true == 1 && false == 0 )) || { echo >&2 ERROR: true=$true false=$false ; return ; }
+_setpath ()
+{
+    [[ -n ${true+_}  ]] || local -ri true=1  || { echo >&2 ERROR cannot set true=1 in _setpath  ; return 2 ; }
+    [[ -n ${false+_} ]] || local -ri false=0 || { echo >&2 ERROR cannot set false=0 in _setpath ; return 2 ; }
+    (( true == 1 && false == 0 )) || { echo >&2 ERROR: true=$true false=$false ; return 2 ; }
     local -ri ALLOW=0 PERMIT=0
     local -ri ALWAYS=1 DO=1 FORCE=1 NEED=1 REQUIRE=1
     local -ri AVOID=-1 DONT=-1 FORBID=-1 NEVER=-1 NO=-1 NOT=-1 PROHIBIT=-1
     local -ri UNEQUAL=2
 
-    local   pathvar= mode=end clear=false arg= orig_arg= elem= xdir= \
+    local   pathvar= mode=end clear=false \
             sep=: prefix= suffix= realpath=false verbose=false \
             if_exist=false if_dir=false if_file=false if_link=false \
-            if_notlink=false xpart pi \
+            if_notlink=false \
             use_hosttype=false move=all xrel= dot=ALLOW \
             quotenext=false quoteall=false
+    local   arg elem orig_arg pi xdir xpart
     local -a path=() xparts=()
 
     if [[ $* = "-h" || "--help" = "$*"* && $* = "--h"* ]]
@@ -36,6 +38,7 @@ Options include:
         These defaults are useful for PATH, LD_LIBRARY_PATH and many others, but
         it can also be used to set directories in CCFLAGS by using --space -I
 EOM
+
         return 0
     fi
 
@@ -47,64 +50,69 @@ EOM
 
         (( quoteall || quotenext)) || {
             case $arg in
-            (-- | --quote-all)          quoteall=true ;;
-            (- | -q | --quote-next)     quotenext=true ;;
-            (--always)                  if_dir=false if_file=false if_link=false if_notlink=false if_exist=false ;;
-            (--dot=[A-Z]*)              dot=${arg#*=} ;;
-            (--empty=[A-Z]*)            dot=-${arg#*=} ;;
-            (--if-dir | -d)             if_dir=true if_exist=false ;;
-            (--if-exists | -e)          if_exist=true ;;
-            (--if-file | -f)            if_file=true if_exist=false ;;
-            (--if-link | -l)            if_link=true if_exist=false if_notlink=false ;;
-            (--if-not-link | -h)        if_notlink=true if_link=false ;;
-            (--move|--move=FORCE)       move=all ;;
-            (--move-if-@(abs|rel))      move=${arg##*[=-]} ;;
-            (--move=NEVER)              move=none ;;
-            (--prefix=*)                prefix=${arg#-*=} ;;
-            (--real-path)               realpath=true ;;
-            (--?(separator=)colon)      sep=':' ;;
-            (--?(separator=)comma)      sep=',' ;;
-            (--?(separator=)semicolon)  sep=';' ;;
-            (--?(separator=)space)      sep=' ' ;;
-            (--separator=*)             printf -v sep %b "${arg#-*=}"
-                                        (( ${#sep} == 1 )) || {
-                                            echo "# $FUNCNAME: Invalid separator '$sep' (not a single character)"
-                                            return 1
-                                        }
-                                        ;;
-            (--no-merge-@(space-dot|dot-space))
-                                        dot=UNEQUAL ;;
-            (--suffix=*)                suffix=${arg#-*=} ;;
-            (--use-host?(-)type)        use_hosttype=true xparts=() ;;
-            (--use-host?(-)type=[A-Z]*) use_hosttype=${arg#*=} ; (( use_hosttype = use_hosttype > 0 )) ; xparts=() ;;
-            (-[A-Z])                    sep=' ' prefix=$arg ;;
+                (-- | --quote-all)          quoteall=true ;;
+                (- | -q | --quote-next)     quotenext=true ;;
+                (--always)                  if_dir=false if_file=false if_link=false if_notlink=false if_exist=false ;;
+                (--dot=[A-Z]*)              dot=${arg#*=} ;;
+                (--empty=[A-Z]*)            dot=-${arg#*=} ;;
+                (--if-dir | -d)             if_dir=true if_exist=false ;;
+                (--if-exists | -e)          if_exist=true ;;
+                (--if-file | -f)            if_file=true if_exist=false ;;
+                (--if-link | -l)            if_link=true if_exist=false if_notlink=false ;;
+                (--if-not-link | -h)        if_notlink=true if_link=false ;;
+                (--move | --move=FORCE)     move=all ;;
+                (--move-if-@(abs|rel))      move=${arg##*[=-]} ;;
+                (--move=NEVER)              move=none ;;
+                (--prefix=*)                prefix=${arg#-*=} ;;
+                (--real-path)               realpath=true ;;
+                (--?(separator=)colon)      sep=':' ;;
+                (--?(separator=)comma)      sep=',' ;;
+                (--?(separator=)semicolon)  sep=';' ;;
+                (--?(separator=)space)      sep=' ' ;;
+                (--separator=*)
+                                            printf -v sep %b "${arg#-*=}"
+                                            (( ${#sep} == 1 )) || {
+                                                echo "# $FUNCNAME: Invalid separator '$sep' (not a single character)"
+                                                return 1
+                                            }
+                                            ;;
+                (--no-merge-@(space-dot|dot-space))
+                                            dot=UNEQUAL ;;
+                (--suffix=*)                suffix=${arg#-*=} ;;
+                (--use-host?(-)type)        use_hosttype=true xparts=() ;;
+                (--use-host?(-)type=[A-Z]*)
+                                            use_hosttype=${arg#*=}
+                                            (( use_hosttype = use_hosttype > 0 ))
+                                            xparts=()
+                                            ;;
+                (-[A-Z])                    sep=' ' prefix=$arg ;;
+                (-[#%^+:,/\;])              sep=${arg:1} ;;
+                (-a | --append)             mode=end xparts=() ;;
+                (-c | --clear)              clear=true xparts=() ;;
+                (-k | --delete)             mode=delete xparts=() ;;
+                (-p | --prefix)             mode=begin xparts=() ;;
+                (-v | --verbose)            verbose=true ;;
 
-            (-[#%^+:,/\;])              sep=${arg:1} ;;
+                ( --@(allow|always|avoid|do|dont|forbid|force|need|never|no|not|permit|prohibit|require)-* )
+                                            pi=${arg#--*-} arg=${arg%-"$pid"} arg=${arg#--}
+                                            set -- "--$pi=${arg^^}" "$@"
+                                            continue ;;
 
-            (-a | --append)             mode=end xparts=() ;;
-            (-c | --clear)              clear=true xparts=() ;;
-            (-k | --delete)             mode=delete xparts=() ;;
-            (-p | --prefix)             mode=begin xparts=() ;;
-            (-v | --verbose)            verbose=true ;;
+                ( --*=@(allow|permit) )     set -- "${arg%%=*}=ALLOW" "$@" ; continue ;;
+                ( --*=@(force|do|need|always|require) )
+                                            set -- "${arg%%=*}=FORCE" "$@" ; continue ;;
+                ( --*=@(never|no|dont|not|prohibit) )
+                                            set -- "${arg%%=*}=NEVER" "$@" ; continue ;;
 
-            ( --@(allow|always|avoid|do|dont|forbid|force|need|never|no|not|permit|prohibit|require)-* )
-                                        pi=${arg#--*-} arg=${arg%-"$pid"} arg=${arg#--}
-                                        set -- "--$pi=${arg^^}" "$@"
-                                        continue ;;
+                ( --*=FORCE )               set "${arg%%=*}" "$@" ; continue ;;
 
-            ( --*=@(allow|permit)                 )  set -- "${arg%%=*}=ALLOW" "$@" ; continue ;;
-            ( --*=@(force|do|need|always|require) )  set -- "${arg%%=*}=FORCE" "$@" ; continue ;;
-            ( --*=@(never|no|dont|not|prohibit)   )  set -- "${arg%%=*}=NEVER" "$@" ; continue ;;
+                (-[^-][^-]*)                set -- "${arg:0:2}" "-${arg:2}" "$@" ;;
 
-            ( --*=FORCE )  set "${arg%%=*}" "$@" ; continue ;;
-
-            (-[^-][^-]*)                set -- "${arg:0:2}" "-${arg:2}" "$@" ;;
-
-            (-*)
-                echo "# $FUNCNAME: Invalid option '$arg'; try '$FUNCNAME --help'" 1>&2
-                return 1
-                ;;
-            (*) false ;;
+                (-*)
+                    echo "# $FUNCNAME: Invalid option '$arg'; try '$FUNCNAME --help'" 1>&2
+                    return 1
+                    ;;
+                (*) false ;;
             esac && {
                 orig_arg=$2
                 continue
@@ -116,13 +124,13 @@ EOM
         then
             pathvar=$arg
             # First time: initialize path from ${!pathvar}
-            IFS="$sep" read -r -a path <<<"${!pathvar}$sep" || path=()
+            IFS="$sep" read -r -a path <<< "${!pathvar}$sep" || path=()
             continue
         fi
 
         case $arg in
-        (.)  (( dot == PROHIBIT )) && arg= ;;
-        ('') (( dot == FORCE    )) && arg=.  ;;
+            ( . )   (( dot == PROHIBIT )) && arg=  ;;
+            ( '' )  (( dot == FORCE    )) && arg=. ;;
         esac
 
         if (( ${#xparts[@]} == 0 ))
@@ -136,11 +144,12 @@ EOM
                 # prospective directory name; for everyone else, just use the
                 # $HOSTTYPE (for now).
                 case $HOSTTYPE in
-              # (i*86)          xparts+=( $(( 32 << ( ${HOSTTYPE:0-3} > 600 ) )) ) ;;
-                (i[3-5]86|ia32) xparts+=( 32 ) ;;
-                (i686*)         xparts+=( 64 ) ;;
-                (x86_+([0-9]))  xparts+=( "${HOSTTYPE#*_}" ) ;;
-                (*)             xparts+=( "$HOSTTYPE" ) ;;
+                  # (i*86)          xparts+=( $(( 32 << ( ${HOSTTYPE:0-3} > 600 ) )) ) ;;
+                    (i[3-5]86 | ia32)
+                                    xparts+=( 32 ) ;;
+                    (i686*)         xparts+=( 64 ) ;;
+                    (x86_+([0-9]))  xparts+=( "${HOSTTYPE#*_}" ) ;;
+                    (*)             xparts+=( "$HOSTTYPE" ) ;;
                 esac
             fi
 
@@ -149,7 +158,8 @@ EOM
 
             # If moving to beginning of path, reverse the list so they still
             # wind up in the path in the right order.
-            if [[ $mode = begin ]]; then
+            if [[ $mode = begin ]]
+            then
                 local -i _n _m=${#xparts[@]}-1
                 for (( _n=0 ; _n<_m-_n ; ++_n )) do
                     _t=${xparts[_n]}
@@ -174,11 +184,14 @@ EOM
             fi
             xdir="$prefix$elem$suffix"
             if (( ${#path[@]} )) &&
-                for pi in "${!path[@]}" ; do
-                    [[ "${path[$pi]}" = "$xdir" ]] &&   # either an exact match
+                for pi in "${!path[@]}"
+                do
+                    # Either: an exact match
+                    [[ "${path[$pi]}" = "$xdir" ]] &&
                         break
-                    [[ "${path[$pi]}$xdir" = . ]] &&    # or, one is "." and the other is "" and
-                        (( !( dot & UNEQUAL ) )) &&     # not treating "." and "" as distinct
+                    # or: one is "." and the other is "", but treating them as equivalent
+                    [[ "${path[$pi]}$xdir" = . ]] &&
+                        (( !( dot & UNEQUAL ) )) &&
                         break
                 done
             then
@@ -210,14 +223,14 @@ EOM
             fi
 
             case $mode in
-            (begin)
-                path=( "$xdir" "${path[@]}" )
-                ;;
-            (delete)
-                ;;
-            (*)
-                path=( "${path[@]}" "$xdir" )
-                ;;
+                (begin)
+                    path=( "$xdir" "${path[@]}" )
+                    ;;
+                (delete)
+                    ;;
+                (*)
+                    path=( "${path[@]}" "$xdir" )
+                    ;;
             esac
         done
         orig_arg=$1
